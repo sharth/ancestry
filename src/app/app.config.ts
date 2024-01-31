a3a69fb5a8be8c3a49e53ffdf60001a9a1826605
William Lynch <william@lynch.us>
2024-01-31 03:41:08 +0000

    Use Hash Location for Github Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { type ApplicationConfig } from '@angular/core'
-import { provideRouter, withComponentInputBinding } from '@angular/router'
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router'
 import { routes } from './app.routes'
 import { provideAnimations } from '@angular/platform-browser/animations'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideAnimations()]
+  providers: [
+    provideRouter(routes, withComponentInputBinding(), withHashLocation()),
+    provideAnimations()
+  ]
 }
