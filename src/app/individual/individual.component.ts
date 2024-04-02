aa332bea26f8e1cdcf6914c6dd69707e31a3932d
William Lynch <william@lynch.us>
2024-04-02 06:17:12 +0000

    Move details about an individual into the individual class rather than in the component.

--- src/app/individual/individual.component.ts
+++ src/app/individual/individual.component.ts
@@ -16,66 +16,9 @@ export class IndividualComponent {
   xref = input.required<string>()
   individual = computed(() => this.ancestryService.individual(this.xref()))
 
-  relatives: Signal<Array<{ relationship: string, individual: GedcomIndividual }>> = computed(() => {
-    const relatives = []
-
-    // Parents and Siblings
-    const family = this.individual().childOfFamily
-    if (family?.husband != null) { relatives.push({ relationship: 'Father', individual: family.husband }) }
-    if (family?.wife != null) { relatives.push({ relationship: 'Mother', individual: family.wife }) }
-    if (family?.children != null) {
-      relatives.push(...family.children
-        .filter(sibling => sibling !== this.individual())
-        .map(sibling => ({
-          relationship: sibling.sex === 'Male' ? 'Brother' : sibling.sex === 'Female' ? 'Sister' : 'Sibling',
-          individual: sibling
-        })))
-    }
-
-    // Stepparents and Stepsiblings
-    for (const parent of family?.parents() ?? []) {
-      for (const stepfamily of parent.parentOfFamilies) {
-        if (stepfamily !== family) {
-          if ((stepfamily.husband != null) && stepfamily.husband !== family?.husband) { relatives.push({ relationship: 'Stepfather', individual: stepfamily.husband }) }
-          if ((stepfamily.wife != null) && stepfamily.wife !== family?.wife) { relatives.push({ relationship: 'Stepmother', individual: stepfamily.wife }) }
-          relatives.push(...stepfamily.children
-            .map(stepsibling => ({
-              relationship: stepsibling.sex === 'Male' ? 'Stepbrother' : stepsibling.sex === 'Female' ? 'Stepsister' : 'Stepsibling',
-              individual: stepsibling
-            })))
-        }
-      }
-    }
-
-    // Spouse and children
-    for (const family of this.individual().parentOfFamilies) {
-      if (family.husband != null && family.husband !== this.individual()) { relatives.push({ relationship: 'Husband', individual: family.husband }) }
-      if (family.wife != null && family.wife !== this.individual()) { relatives.push({ relationship: 'Wife', individual: family.wife }) }
-      relatives.push(...family.children
-        .map(child => ({
-          relationship: child.sex === 'Male' ? 'Son' : child.sex === 'Female' ? 'Daughter' : 'Child',
-          individual: child
-        })))
-    }
-
-    return relatives
-  })
-
-  ancestors: Signal<Array<GedcomIndividual | undefined>> = computed(() => {
-    const ancestors = new Array<GedcomIndividual | undefined>()
-    ancestors[1] = this.individual()
-    for (let i = 1; i < ancestors.length; i += 1) {
-      if (ancestors[i] != null) {
-        ancestors[2 * i + 0] = ancestors[i]?.childOfFamily?.husband
-        ancestors[2 * i + 1] = ancestors[i]?.childOfFamily?.wife
-      }
-    }
-    return ancestors
-  })
-
   censusTable = computed(() => {
     const yearSet = new Set<string>()
-    for (const ancestor of this.ancestors()) {
+    for (const ancestor of this.individual().ancestors()) {
       if (ancestor != null) {
         for (const event of ancestor.censusEvents()) {
           if (event.date != null) {
@@ -90,7 +33,7 @@ export class IndividualComponent {
       dateToColumn.set(date, dateToColumn.size)
     }
     const rows = []
-    for (const ancestor of this.ancestors()) {
+    for (const ancestor of this.individual().ancestors()) {
       if (ancestor != null) {
         const row = {
           ancestor,
