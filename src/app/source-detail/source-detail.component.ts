0f8f50c85bc01c7155251a256dfcbf9cfabaa48d
William Lynch <william@lynch.us>
2024-04-17 23:49:31 -0700

    Include citations

--- src/app/source-detail/source-detail.component.ts
+++ src/app/source-detail/source-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, input } from '@angular/core'
 import { AncestryService } from '../ancestry.service'
 import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common'
+import { GedcomCitation, GedcomEvent, GedcomIndividual } from '../../gedcom'
 
 @Component({
   selector: 'app-source-detail',
@@ -14,4 +15,29 @@ export class SourceDetailComponent {
   ancestryService = inject(AncestryService)
   xref = input.required<string>()
   source = computed(() => this.ancestryService.source(this.xref()))
+
+  citations(): { individual: GedcomIndividual; event: GedcomEvent; citation: GedcomCitation; }[] {
+    const arr = []
+    for (const individual of this.ancestryService.individuals()) {
+      for (const event of individual.events) {
+        for (const citation of event.citations) {
+          if (citation.source == this.source()) {
+            arr.push({ individual: individual, event: event, citation: citation })
+          }
+        }
+      }
+    }
+    return arr
+  }
 }
+
+// individualsBySurname(): Map<string | undefined, GedcomIndividual[]> {
+//   const surnameMap = new Map<string, GedcomIndividual[]>()
+//   for (const individual of this.ancestryService.individuals()) {
+//     let surnames = surnameMap.get(individual.surname ?? '')
+//     if (surnames == null) surnameMap.set(individual.surname ?? '', surnames = [])
+//     surnames.push(individual)
+//   }
+//   return surnameMap
+// }
+
