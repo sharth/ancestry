5012f2abe71ba67202550fbc298fe8f12fb5dc54
William Lynch <william@lynch.us>
2024-06-05 00:40:50 -0700

    Fix scrolling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import {type ApplicationConfig, provideExperimentalZonelessChangeDetection} from '@angular/core';
-import {provideRouter, withComponentInputBinding, withHashLocation} from '@angular/router';
+import {provideRouter, withComponentInputBinding, withHashLocation, withInMemoryScrolling} from '@angular/router';
 import {routes} from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding(), withHashLocation()),
+    provideRouter(routes, withComponentInputBinding(), withHashLocation(), withInMemoryScrolling({
+      scrollPositionRestoration: 'enabled',
+    })),
     provideExperimentalZonelessChangeDetection(),
     provideAnimations(),
   ],
