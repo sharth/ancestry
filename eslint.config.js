21255da1aad4598c9315f10dfe1159a2579a3713
William Lynch <william@lynch.us>
2024-06-05 22:32:07 -0700

    Thoughts on progress

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,17 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import google from "eslint-config-google";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import google from 'eslint-config-google';
+import markdown from 'eslint-plugin-markdown';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  google,
-  {
-    rules: {
-      "require-jsdoc": "off",
-      "max-len": ["error", { "code": 120 }]
-    }
-  }
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...markdown.configs.recommended,
+    google,
+    {
+      rules: {
+        'require-jsdoc': 'off',
+        'max-len': ['error', {'code': 120}],
+      },
+    },
 );
