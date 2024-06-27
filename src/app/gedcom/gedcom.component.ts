567750ac176483ceddf1812f08f5c2e79feba2b7
William Lynch <william@lynch.us>
2024-06-27 00:12:36 -0700

    Print gedcom differences.

--- src/app/gedcom/gedcom.component.ts
+++ src/app/gedcom/gedcom.component.ts
@@ -1,5 +1,7 @@
 import {Component, computed, inject} from '@angular/core';
 import {AncestryService} from '../ancestry.service';
+import type {GedcomRecord} from '../../gedcom/gedcomRecord';
+import {parseGedcomRecordsFromText} from '../../gedcom/gedcomRecord';
 
 @Component({
   selector: 'app-gedcom',
@@ -10,7 +12,32 @@ import {AncestryService} from '../ancestry.service';
 })
 export class GedcomComponent {
   ancestryService = inject(AncestryService);
-  gedcomText = computed(() => this.ancestryService.gedcomRecords()
-      .flatMap((gedcomRecord) => gedcomRecord.text())
-      .join('\n'));
+
+  differences = computed(() => {
+    const oldGedcomText = Array.from(parseGedcomRecordsFromText(this.ancestryService.originalGedcomText()))
+        .map((record) => record.text().join('\n'));
+    const newGedcomText = this.ancestryService.gedcomRecords()
+        .map((record) => record.text().join('\n'));
+
+    const differences = [];
+    let oldGedcomIndex = 0;
+    let newGedcomIndex = 0;
+    while (newGedcomIndex < newGedcomText.length) {
+      if (oldGedcomText[oldGedcomIndex] == newGedcomText[newGedcomIndex]) {
+        oldGedcomIndex += 1;
+        newGedcomIndex += 1;
+      } else if (oldGedcomText.includes(newGedcomText[newGedcomIndex], oldGedcomIndex)) {
+        differences.push({oldText: oldGedcomText[oldGedcomIndex], newText: ''});
+        oldGedcomIndex += 1;
+      } else {
+        differences.push({oldText: '', newText: newGedcomText[newGedcomIndex]});
+        newGedcomIndex += 1;
+      }
+    }
+    while (oldGedcomIndex < oldGedcomText.length) {
+      differences.push({oldText: oldGedcomText[oldGedcomIndex], newText: ''});
+      oldGedcomIndex += 1;
+    }
+    return differences;
+  });
 }
