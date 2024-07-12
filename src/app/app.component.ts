7e9f5396e41da20dacd0bccd24674c37be77f37c
William Lynch <william@lynch.us>
2024-07-12 16:18:21 -0700

    Use showOpenFilePicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,26 +20,17 @@ export class AppComponent {
     }
   }
 
-  gedcomFileChanged(el: EventTarget): void {
-    if (!(el instanceof HTMLInputElement)) {
-      throw new Error(`Expected el to be an HTMLInputElement, was ${el?.constructor?.name || el}`);
-    }
-    if (!(el.files instanceof FileList)) {
-      throw new Error(`Expected el.files to be a FileList, was ${el?.constructor?.name || el}`);
-    }
 
-    if (el.files.length === 0) {
-      this.parseSomeText('');
-    } else if (el.files.length === 1) {
-      this.parseSomeFile(el.files[0]);
-    } else {
-      throw new Error(`Expected el.files.length to be <= 1, was ${el.files.length}`);
-    }
-  }
-
-  async parseSomeFile(file: File): Promise<void> {
-    const response = await file.text();
-    this.parseSomeText(response);
+  async openFile(): Promise<void> {
+    const [fileHandle] = await window.showOpenFilePicker({
+      types: [{
+        description: 'Gedcom Files',
+        accept: {'text/plain': ['.ged']},
+      }],
+    });
+    const file = await fileHandle.getFile();
+    const text = await file.text();
+    this.parseSomeText(text);
   }
 
   parseSomeText(text: string): void {
