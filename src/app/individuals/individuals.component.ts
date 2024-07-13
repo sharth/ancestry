56ed896bae67eb200c4abc7f0e5a2b8df8bc7f66
William Lynch <william@lynch.us>
2024-07-13 10:34:36 -0700

    cleanup individuals.. kinda.

--- src/app/individuals/individuals.component.ts
+++ src/app/individuals/individuals.component.ts
@@ -2,7 +2,6 @@ import {Component, computed, inject} from '@angular/core';
 import {CommonModule, KeyValuePipe} from '@angular/common';
 import {RouterLink} from '@angular/router';
 import {AncestryService} from '../ancestry.service';
-import type {GedcomIndividual} from '../../gedcom/gedcomIndividual';
 
 @Component({
   selector: 'app-individuals',
@@ -12,21 +11,18 @@ import type {GedcomIndividual} from '../../gedcom/gedcomIndividual';
   styleUrl: './individuals.component.css',
 })
 export class IndividualsComponent {
-  ancestryService = inject(AncestryService);
+  readonly ancestryService = inject(AncestryService);
 
-  readonly individuals = computed(() => {
-    const individuals = [...this.ancestryService.individuals().values()];
-    individuals.sort((a, b) => a.xref.localeCompare(b.xref));
-    return individuals;
-  });
+  readonly individuals = computed(() =>
+    this.ancestryService.individuals().toList()
+        .sort((a, b) => a.xref.localeCompare(b.xref)));
 
-  readonly individualsBySurname = computed<Map<string | undefined, GedcomIndividual[]>>(() => {
-    const surnameMap = new Map<string, GedcomIndividual[]>();
-    for (const individual of this.ancestryService.individuals().values()) {
-      let surnames = surnameMap.get(individual.surname ?? '');
-      if (surnames == null) surnameMap.set(individual.surname ?? '', surnames = []);
-      surnames.push(individual);
-    }
-    return surnameMap;
-  });
+  readonly individualsBySurname = computed(() =>
+    this.ancestryService.individuals().valueSeq()
+        .groupBy((individual) => individual.surname ?? '')
+        .mapEntries(([surname, individuals]) =>
+          [surname, individuals.sort((a, b) => (a.name ?? '').localeCompare(b.name ?? ''))])
+        .entrySeq()
+        .map((entry) => ({surname: entry[0], individuals: entry[1]}))
+        .sort((a, b) => a.surname.localeCompare(b.surname)));
 }
