e555962949deec5cf57f907b796dd70ef078fa7b
William Lynch <william@lynch.us>
2024-07-28 08:47:01 -0700

    Add a repositories table as well.

--- src/database/ancestry.database.ts
+++ src/database/ancestry.database.ts
@@ -1,14 +1,19 @@
 import {Dexie} from 'dexie';
 import {GedcomSource} from '../gedcom/gedcomSource';
+import {GedcomRepository} from '../gedcom/gedcomRepository';
 
 class AncestryDatabase extends Dexie {
+  repositories!: Dexie.Table<GedcomRepository, string>;
   sources!: Dexie.Table<GedcomSource, string>;
 
   constructor() {
     super('AncestryDatabase');
     this.version(1).stores({
+      repositories: 'xref',
       sources: 'xref',
     });
+
+    this.repositories.mapToClass(GedcomRepository);
     this.sources.mapToClass(GedcomSource);
   }
 }
