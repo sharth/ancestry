36dae01e611b2c281e69127a14721bba1d7c7680
William Lynch <william@lynch.us>
2024-08-17 09:21:04 -0700

    simplify the form

--- src/app/source/source.component.ts
+++ src/app/source/source.component.ts
@@ -4,11 +4,6 @@ import {Component, input, viewChild} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {SourceEditAbbrComponent} from './source-edit-abbr.component';
-import {SourceEditTitleComponent} from './source-edit-title.component';
-import {SourceEditTextComponent} from './source-edit-text.component';
-import {SourceEditRepositoryCitationsComponent} from './source-edit-repository-citations.component';
-import {SourceEditUnknownsComponent} from './source-edit-unknowns.component';
 import {serializeGedcomRecordToText} from '../../gedcom/gedcomRecord.serializer';
 import {serializeGedcomSourceToGedcomRecord} from '../../gedcom/gedcomSource.serializer';
 import {toObservable} from '@angular/core/rxjs-interop';
@@ -22,16 +17,7 @@ import type { GedcomSource } from '../../gedcom/gedcomSource';
   standalone: true,
   templateUrl: './source.component.html',
   styleUrl: './source.component.css',
-  imports: [
-    CommonModule,
-    RouterModule,
-    FormsModule,
-    SourceEditAbbrComponent,
-    SourceEditTitleComponent,
-    SourceEditTextComponent,
-    SourceEditRepositoryCitationsComponent,
-    SourceEditUnknownsComponent,
-  ],
+  imports: [CommonModule,RouterModule, FormsModule],
 })
 export class SourceComponent {
   readonly xref = input.required<string>();
@@ -64,25 +50,45 @@ export class SourceComponent {
           gedcom: serializeGedcomRecordToText(unknownRecord),
         })),
         gedcom: serializeGedcomRecordToText(serializeGedcomSourceToGedcomRecord(source)),
+        repositories,
       }
     }),
   );
 
-  readonly model$ = signal<GedcomSource | undefined>(undefined);
+  readonly formModel = signal<GedcomSource | undefined>(undefined);
   readonly editDialog = viewChild.required<ElementRef<HTMLDialogElement>>('editDialog');
+
+
+  addRepositoryCitation() {
+    this.formModel()?.repositoryCitations.push({
+      repositoryXref: '',
+      callNumbers: [''],
+    });
+  }
+
+  removeRepositoryCitation(index: number) {
+    this.formModel()?.repositoryCitations.splice(index, 1);
+  }
+
+  removeUnknownRecord(index: number) {
+    this.formModel()?.unknownRecords.splice(index, 1);
+  }
 
   openForm() {
     void rxjs.firstValueFrom(this.source$).then((source) => {
+      console.log(source);
       if (source == null) throw new Error();
-      this.model$.set(source);
+      this.formModel.set(source);
       this.editDialog().nativeElement.showModal();
     })
   }
 
   submitForm() {
-    const model = this.model$();
-    if (model != null)
-      void ancestryDatabase.sources.put(model);
+    const source = this.formModel();
+    if (source != null)
+      void ancestryDatabase.sources.put(source);
     this.editDialog().nativeElement.close();
   }
+
+  serializeGedcomRecordToText = serializeGedcomRecordToText;
 }
