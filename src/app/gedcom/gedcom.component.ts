94fe77f3a0c358805feca0838527ade68984a911
William Lynch <william@lynch.us>
2024-12-02 22:24:46 -0800

    cleanup how we generate the diff a bit.

--- src/app/gedcom/gedcom.component.ts
+++ src/app/gedcom/gedcom.component.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, computed, resource } from "@angular/core";
 import { ancestryDatabase } from "../../database/ancestry.database";
-import * as dexie from "dexie";
-import * as rxjs from "rxjs";
 import { GedcomDiffComponent } from "../../util/gedcom-diff.component";
 import type { GedcomRecord } from "../../gedcom";
 import {
@@ -13,10 +11,10 @@ import {
   serializeGedcomRecordToText,
   serializeGedcomRepository,
   serializeGedcomTrailer,
+  serializeGedcomSubmitter,
   serializeGedcomMultimedia,
   parseGedcomRecords,
 } from "../../gedcom";
-import { serializeGedcomSubmitter } from "../../gedcom/gedcomSubmitterSerializer";
 
 @Component({
   selector: "app-gedcom",
@@ -26,102 +24,74 @@ import { serializeGedcomSubmitter } from "../../gedcom/gedcomSubmitterSerializer
   styleUrl: "./gedcom.component.css",
 })
 export class GedcomComponent {
-  readonly vm$ = rxjs
-    .combineLatest([this.oldGedcomText(), this.orderedNewGedcomText()])
-    .pipe(
-      rxjs.map(([oldGedcomText, newGedcomText]) => ({
-        oldGedcomText: oldGedcomText.join("\n"),
-        newGedcomText: newGedcomText.join("\n"),
-      }))
+  private readonly database = resource({
+    request: () => ({
+      ancestryDatabaseIteration: ancestryDatabase.iteration(),
+    }),
+    loader: async ({ request }) => ({
+      originalText: await ancestryDatabase.originalText.toArray(),
+      headers: await ancestryDatabase.headers.toArray(),
+      submitters: await ancestryDatabase.submitters.toArray(),
+      individuals: await ancestryDatabase.individuals.toArray(),
+      families: await ancestryDatabase.families.toArray(),
+      sources: await ancestryDatabase.sources.toArray(),
+      repositories: await ancestryDatabase.repositories.toArray(),
+      multimedia: await ancestryDatabase.multimedia.toArray(),
+      trailers: await ancestryDatabase.trailers.toArray(),
+    }),
+  });
+
+  private readonly originalGedcomText = computed<string[]>(() => {
+    const originalText = this.database.value()?.originalText;
+    if (originalText == undefined) {
+      return [];
+    }
+    return originalText.flatMap((row) => row.text.split(/\r?\n/));
+  });
+
+  private readonly originalGedcomRecords = computed<GedcomRecord[]>(() =>
+    parseGedcomRecords(this.originalGedcomText().join("\n"))
+  );
+
+  private readonly currentGedcomRecords = computed<GedcomRecord[]>(() => {
+    const database = this.database.value();
+    if (database === undefined) return [];
+    return [
+      ...database.headers.map(serializeGedcomHeader),
+      ...database.submitters.map(serializeGedcomSubmitter),
+      ...database.individuals.map(serializeGedcomIndividual),
+      ...database.families.map(serializeGedcomFamily),
+      ...database.sources.map(serializeGedcomSource),
+      ...database.repositories.map(serializeGedcomRepository),
+      ...database.multimedia.map(serializeGedcomMultimedia),
+      ...database.trailers.map(serializeGedcomTrailer),
+    ];
+  });
+
+  private readonly orderedGedcomRecords = computed<GedcomRecord[]>(() => {
+    const asKey = (record: GedcomRecord) =>
+      `${record.tag} ${record.xref ?? ""} ${record.value ?? ""}`;
+    const orderedRecords = new Map<string, GedcomRecord[]>();
+    this.originalGedcomRecords()
+      .filter((record) => record.abstag != "TRLR")
+      .forEach((record) => {
+        orderedRecords.set(asKey(record), []);
+      });
+    this.currentGedcomRecords().forEach((record) => {
+      if (!orderedRecords.get(asKey(record))?.push(record))
+        orderedRecords.set(asKey(record), [record]);
+    });
+    return Array.from(orderedRecords.values()).flat();
+  });
+
+  private readonly orderedGedcomText = computed<string[]>(() => {
+    return this.orderedGedcomRecords().flatMap((record) =>
+      serializeGedcomRecordToText(record)
     );
+  });
 
-  oldGedcomText(): rxjs.Observable<string[]> {
-    return rxjs
-      .from(dexie.liveQuery(() => ancestryDatabase.originalText.toArray()))
-      .pipe(
-        rxjs.map((rows) => {
-          console.log("compute oldGedcomText");
-          if (rows.length == 0) {
-            return [];
-          } else if (rows.length == 1) {
-            return rows[0].text.split(/\r?\n/);
-          } else {
-            throw new Error(
-              "Unexpected number of rows in ancestryDatabase.originalText"
-            );
-          }
-        })
-      );
-  }
-
-  oldGedcomRecords(): rxjs.Observable<GedcomRecord[]> {
-    return this.oldGedcomText().pipe(
-      rxjs.map((oldGedcomText) => parseGedcomRecords(oldGedcomText.join("\n")))
-    );
-  }
-
-  newGedcomRecords(): rxjs.Observable<GedcomRecord[]> {
-    return rxjs
-      .combineLatest([
-        rxjs
-          .from(dexie.liveQuery(() => ancestryDatabase.headers.toArray()))
-          .pipe(rxjs.map((headers) => headers.map(serializeGedcomHeader))),
-        rxjs
-          .from(dexie.liveQuery(() => ancestryDatabase.submitters.toArray()))
-          .pipe(rxjs.map((headers) => headers.map(serializeGedcomSubmitter))),
-        rxjs
-          .from(dexie.liveQuery(() => ancestryDatabase.individuals.toArray()))
-          .pipe(rxjs.map((headers) => headers.map(serializeGedcomIndividual))),
-        rxjs
-          .from(dexie.liveQuery(() => ancestryDatabase.families.toArray()))
-          .pipe(rxjs.map((headers) => headers.map(serializeGedcomFamily))),
-        rxjs
-          .from(dexie.liveQuery(() => ancestryDatabase.sources.toArray()))
-          .pipe(rxjs.map((headers) => headers.map(serializeGedcomSource))),
-        rxjs
-          .from(dexie.liveQuery(() => ancestryDatabase.repositories.toArray()))
-          .pipe(rxjs.map((headers) => headers.map(serializeGedcomRepository))),
-        rxjs
-          .from(dexie.liveQuery(() => ancestryDatabase.multimedia.toArray()))
-          .pipe(rxjs.map((headers) => headers.map(serializeGedcomMultimedia))),
-        rxjs
-          .from(dexie.liveQuery(() => ancestryDatabase.trailers.toArray()))
-          .pipe(rxjs.map((headers) => headers.map(serializeGedcomTrailer))),
-      ])
-      .pipe(rxjs.map((records: GedcomRecord[][]) => records.flat()));
-  }
-
-  // Current set of GedcomRecords, but organized similarly to the text that came in from the user.
-  orderedNewGedcomRecords(): rxjs.Observable<GedcomRecord[]> {
-    return rxjs
-      .combineLatest([this.newGedcomRecords(), this.oldGedcomRecords()])
-      .pipe(
-        rxjs.map(([newGedcomRecords, oldGedcomRecords]) => {
-          const orderedRecords = new Map<string, GedcomRecord[]>();
-          oldGedcomRecords
-            .filter((gedcomRecord) => gedcomRecord.abstag != "TRLR")
-            .forEach((gedcomRecord) => {
-              const order = `${gedcomRecord.tag} ${gedcomRecord.xref ?? ""} ${gedcomRecord.value ?? ""}`;
-              orderedRecords.set(order, []);
-            });
-          newGedcomRecords.forEach((gedcomRecord) => {
-            const order = `${gedcomRecord.tag} ${gedcomRecord.xref ?? ""} ${gedcomRecord.value ?? ""}`;
-            if (!orderedRecords.get(order)?.push(gedcomRecord))
-              orderedRecords.set(order, [gedcomRecord]);
-          });
-
-          return Array.from(orderedRecords.values()).flat();
-        })
-      );
-  }
-
-  orderedNewGedcomText(): rxjs.Observable<string[]> {
-    return this.orderedNewGedcomRecords().pipe(
-      rxjs.map((gedcomRecords: GedcomRecord[]) =>
-        gedcomRecords.flatMap((gedcomRecord: GedcomRecord) =>
-          serializeGedcomRecordToText(gedcomRecord)
-        )
-      )
-    );
-  }
+  readonly vm = computed(() => ({
+    oldGedcomText: this.originalGedcomText().join("\n"),
+    newGedcomText: this.orderedGedcomText().join("\n"),
+  }));
 }
