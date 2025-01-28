ea9897ad76111dc2d6e56553323a816be8dcccab
William Lynch <william@lynch.us>
2025-01-27 22:49:41 -0800

    order quality before data.

--- src/gedcom/gedcomCitationSerializer.ts
+++ src/gedcom/gedcomCitationSerializer.ts
@@ -13,6 +13,7 @@ export function serializeGedcomCitation(
       { tag: "NAME", abstag: "", value: gedcomCitation.name, children: [] },
       { tag: "NOTE", abstag: "", value: gedcomCitation.note, children: [] },
       { tag: "PAGE", abstag: "", value: gedcomCitation.page, children: [] },
+      { tag: "QUAY", abstag: "", value: gedcomCitation.quality, children: [] },
       {
         tag: "DATA",
         abstag: "",
@@ -20,7 +21,6 @@ export function serializeGedcomCitation(
           { tag: "TEXT", abstag: "", value: gedcomCitation.text, children: [] },
         ].filter((record) => record.children.length || record.value),
       },
-      { tag: "QUAY", abstag: "", value: gedcomCitation.quality, children: [] },
     ].filter((record) => record.children.length || record.value),
   };
 }
